314e4a6ede459dd1d26959129b1340ef1de3a355
Thor Bröndum Eklund <thor.bde@gmail.com>
2025-11-09 17:14:15 +0100

    add custom domainä

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thorbde.github.io",
-  base: "/oumaimasimon_frontend",
+  site: "https://oumaimasimon.se",
 });
